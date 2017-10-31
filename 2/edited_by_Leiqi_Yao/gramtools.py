673d3f40e9143bde70431071d29f496e09d2b7cc
Sar-Kerson <512529447@qq.com>
2017-10-31 22:33:16 +0800

    base grammer, with first set

--- 2/edited_by_Leiqi_Yao/gramtools.py
+++ 2/edited_by_Leiqi_Yao/gramtools.py
@@ -67,86 +67,6 @@ def first_set_exp(G, s, firsts):
 	return tup
 
 
-
-def follow_set(G):
-    '''
-    Takes in Grammar as paramater 
-    returns the follow  set for each non terminal in G.variables
-    '''
-    follows = {}
-    #this is the dict which will contain the mappings for n_term to the corresponding follow set (viz a list)
-    firsts = first_set_elem(G)
-    #dict of first set
-    g_prods = G.productions
-    #production rules of the grammar
-#    follows[G.start].add('$')
-#    #merge the frisr set into the follow set
-#    for V in g_prods:
-#        for each_prod in g_prods[V]:
-#            if len(each_prod) > 0:
-#                if each_prod[-1] in G.variables:
-#                    for first in firsts:
-#                        follows[V].add(first)
-                
-    def follow_r(G, n_term, follows):
-        '''
-        This is the recursive follow set function, pass the Grammar G, non terminal n_term (string) and  the follows set (dict) of the grammar
-        if the follow set is already present in the follows dict, then this will just return
-        else it will recursively find all the elements in the follow set for the given n_term, and updates follows
-        '''
-        #if n_term in follows.keys():#problem
-            #return None
-            #if n_term already exists in follows, nothing to update. end function.
-        
-        follows[n_term] = []
-        #n_term does not exist in follows, create an entry for it and populate it.
-        if n_term == G.start:
-            follows[n_term] = ['$']
-
-        for lhs in g_prods:
-            #this for loop takes every non term in the lhs of G.productions, and runs the following operations on it.
-
-            for rule in g_prods[lhs]:
-                #for every production of the lhs (non terminal) in question
-                symbols = rule.split(" ")
-                #split the rhs of the production into list of individual symbols (non terminals and tokens)
-                l = [i for i,x in enumerate(symbols) if x == n_term]
-                #l is the list of indices where n_term appears in the production rule
-                for pos in l:
-                    if pos == len(symbols)-1:
-                        #if n_term has appeared in the end of the production
-                        if lhs not in follows.keys():
-                            follow_r(G, lhs, follows)
-                        follows[n_term].extend(follows[lhs])
-                        #then the follow of lhs is subset of follow of n_term, 
-                        #before this we updated follows with follow set of lhs
-                    else:
-                        #lhs has occured somewhere in between the production 
-                        s = " ".join(symbols[pos+1:])
-                        #get the part of the production which occurs after n_term
-                        f = first_set_exp(G, s, firsts)
-                        #find the first set of that part of production
-                        if '' in f:
-                            #if lambda production exists
-                            f.remove('')
-                            follows[n_term].extend(f)
-                            #then also add follow of lhs to follows of n_term
-                            if lhs not in follows.keys():
-                                follow_r(G, lhs, follows)
-                            follows[n_term].extend(follows[lhs])
-                        else:
-                            #else just add the first set to the follows of n_term
-                            follows[n_term].extend(f)
-        follows[n_term] = list(set(follows[n_term]))
-        #remove any repeating symbols in the follow set of n_term
-
-
-    for n_term in G.variables:
-        follow_r(G, n_term, follows)
-
-    return follows
-
-
 def get_grammar(string):
     '''
     function takes in lines of the grammar rules as input
@@ -203,8 +123,4 @@ F : int | ( E )'''
     print''
     print "First set : "
     print fr
-    print ''
-    fl = follow_set(g)
-    print "Follow Set : "
-    print fl
     
